98d834e2c9130856c890a37a3959e4d1692da9d3
Ishiban10 <95513685+Ishiban10@users.noreply.github.com>
2022-03-31 17:25:46 -0700

    Add files via upload
    Code Journal #1, Prompt #2

---
+++ CJ1p2.py
@@ -0,0 +1,26 @@
+# Code Journal #1 Prompt #2
+
+def sumFloat(flt1, flt2):
+    sum = flt1 + flt2
+    print(sum)
+    print(type(sum))
+
+
+def difInt(int1, int2):
+    dif = int1-int2
+    print(dif)
+    print(type(dif))
+
+def prodintflt(int1, flt1):
+    prod = int1 * flt1
+    print(prod)
+    print(type(prod))
+
+def main():
+    sumFloat(3.2, 4.3)
+    difInt(43, 12)
+    prodintflt(4, 8.9)
+
+
+if __name__ == "__main__":
+    main()
