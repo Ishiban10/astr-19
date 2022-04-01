7703889f80533f13ce0fa851e5a5f873e8b4b44a
Ishiban10 <95513685+Ishiban10@users.noreply.github.com>
2022-03-31 17:09:28 -0700

    Add files via upload
    Code Journal #1, Prompt #1

---
+++ CJ1p1.py
@@ -0,0 +1,11 @@
+# Code Journal #1 Prompt #1
+
+def personalDescription():
+    print("My name is Ishaan Das. \nMy pronouns are he/him. \nMy favorite movie is Up. \nMy favorite food is chicken and rice.")
+
+
+def main():
+    return personalDescription()
+
+if __name__ == "__main__":
+    main()
