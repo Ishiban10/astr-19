257b30e4f6b81983fa5c1a86622839ebbbba426f
Ishiban10 <95513685+Ishiban10@users.noreply.github.com>
2022-04-04 15:51:45 -0700

    Add files via upload
    4/4/22
    Code Journal #1 - Prompt 3

---
+++ CJ1p3.py
@@ -0,0 +1,15 @@
+# Code Journal #1 - Prompt #3
+
+def f(x):
+    return (x ** 3) + 8
+
+
+def main():
+    x = 9
+    print(f(x))
+    if (f(x)) > 27:
+        print("YAY")
+
+
+if __name__ == "__main__":
+    main()
